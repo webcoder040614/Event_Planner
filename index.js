a455241faffeb5d4a74ad979cb81892f1b8c2d8c
webcoder040614 <webcode040614@gmail.com>
2021-10-01 12:18:49 +0530

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const mainbutton = document.getElementById("mainbutton");
+const maincontainer = document.getElementById("maincontainer");
+const card = document.getElementById("card");
+const mybutton = document.getElementById("mybutton");
+const mybutton2 = document.getElementById("mybutton2");
+const close = document.getElementById("close");
+
+mainbutton.addEventListener("click",popupbox);
+close.addEventListener("click",closebox);
+mybutton.addEventListener("click",flipcard);
+mybutton2.addEventListener("click",flipcardback);
+
+function popupbox(){
+    maincontainer.style.display = "block";
+}
+function flipcard(){
+    card.classList.toggle("flipcard");
+}
+function flipcardback(){
+    card.classList.toggle("flipcard");
+}
+function closebox(){
+    maincontainer.style.display = "none";
+}
