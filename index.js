f1a1cec1d6f0a7fd824c44a41c395f91a2b5357b
webcoder040614 <webcode040614@gmail.com>
2021-10-11 17:02:09 +0530

    Login

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const CustBtn = document.getElementById("CustBtn");
 const maincontainer = document.getElementById("maincontainer");
 const close = document.getElementById("close");
-const signupbtn = document.getElementById("signupbtn");
 const card = document.getElementById("card");
-const closeback = document.getElementById("closeback");
+const back_close = document.getElementById("back_close");
+const bg_image = document.getElementById("bg_image");
+const signupbtn = document.getElementById("signupbtn");
+const Signinbtn = document.getElementById("Signinbtn");
 
 CustBtn.addEventListener("click",popupbox);
 close.addEventListener("click",closebox);
-closeback.addEventListener("click",closebox);
+back_close.addEventListener("click",closebox);
 signupbtn.addEventListener("click",flipcard);
+Signinbtn.addEventListener("click",flipcard);
 
 function popupbox(){
     maincontainer.style.display = "block";
+    /* Add the blur effect */
+    bg_image.style.filter = "blur(5px)";
+
 }
 function closebox(){
     maincontainer.style.display = "none";
+    bg_image.style.filter = "none";
 }
 function flipcard(){
     card.classList.toggle("flipcard");
