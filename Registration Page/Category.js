708075d2a8263fd6f0223786fa6144e36586017d
webcoder040614 <webcode040614@gmail.com>
2021-11-07 12:01:17 +0530

    Home

--- Registration Page/Category.js
+++ Registration Page/Category.js
@@ -1,4 +1,4 @@
-document.querySelector('.select-feild').addEventListener('click',()=>{
+document.querySelector('.select-category').addEventListener('click',()=>{
     document.querySelector('.list').classList.toggle('show');
     document.querySelector('.down-arrow').classList.toggle('rotate180');
 })
